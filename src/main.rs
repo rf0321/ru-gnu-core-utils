65a46b0d296ddd6e556638e9c29c672fd568afb6
ItinoseSan <jkekun@gmail.com>
2017-12-28 15:45:20 +0900

    implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#[macro_use]
+extern crate nom; //  use parser combinator lib
+use nom::{IResult, space, alpha};
 use std::env;
 use std::io;
 use std::fs;
+use std::mem;
+
 
 // get current_directory
 fn get_pwd(){    
@@ -13,9 +18,54 @@ fn get_ls(){  // get current dir and file name
         println!("{}",path.unwrap().path().display());
     }
 }
-fn do_mkdir(name: String){ /*make directory. fn argument  is "mkdir <dirname>"*/
+fn do_mkdir(name: &str){ /*make directory. fn argument  is "mkdir <dirname>"*/
     fs::create_dir(name);
 }
-fn main() {
+//  parse the mkdir key word
+named!(name_parser<&str>,
+    chain!(
+        tag!("mkdir") ~
+        space? ~
+        operand: map_res!(
+            alpha,
+            std::str::from_utf8
+        ) ~
+        tag!("") ,
+        || operand
+    )
+);
+fn parsing_operand_expression(input: &str) {
+    match name_parser(input.as_bytes()) {
+        IResult::Done(_, operand) => do_mkdir(operand),
+		IResult::Error(error) => println!("Error: {:?}", error),
+        IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
+    }
+}
+fn parsing_nonoperand_expression(input: &str){
+	match input {
+	      "ls"  => get_ls(),
+	      "pwd"  => get_pwd(),
+		    _ => println!("command is not found or umimplemanted now")
+	}
+}
+fn string_to_static_str(s: String) -> &'static str { //  Convert String to &str for parse input
+    unsafe {
+        let ret = mem::transmute(&s as &str);
+        mem::forget(s);
+		ret
+    }
+}
+fn main(){
+	loop {
+		let mut standard_input = String::new();
+		io::stdin().read_line(&mut standard_input).expect("Failed to read line");
+		let input_to_parser: &str = string_to_static_str(standard_input);
 
+		if input_to_parser.starts_with("mkdir"){
+			parsing_operand_expression(input_to_parser);
+		}
+		else {
+			parsing_nonoperand_expression(input_to_parser);
+		}
+	}
 }
