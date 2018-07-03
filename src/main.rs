b568c15d77d70df993286a932a324193ff38d774
ItinoseSan <jkekun@gmail.com>
2018-07-03 18:58:45 +0900

    [test] show commit

--- src/main.rs
+++ src/main.rs
@@ -2,63 +2,14 @@
 #[allow(unused_must_use)]
 #[warn(unused_must_use)]
 extern crate nom; //  use parser combinator lib
-extern crate reqwest;
-
-use std::io::Read;
+extern crate token;
 use nom::{ IResult };
 use std::io;
 use std::mem;
-use std::fs;
-use std::fs::File;
-use std::env;
 
-fn do_cat(filename: &str){
-    let mut s = String::new();
-    File::open(filename).unwrap().read_to_string(&mut s).unwrap();
-    println!("{}",s);
-}
-fn curl_get_request(url: &str){
-    let mut response = reqwest::get(url).unwrap();
-    let mut s = String::new();
-    response.read_to_string(&mut s);
-    println!("{}",s);
-}
-// get current_directory
-fn get_pwd(){    
-    let path = std::env::current_dir().unwrap();
-    println!("{}",path.display());    
-}
-fn get_ls(){  // get current dir and file name
-    let paths = std::fs::read_dir("./").unwrap();
-    for path in paths {
-        println!("{}",path.unwrap().path().display());
-    }
-}
-fn do_mkdir(name: &str){ /*make directory. fn argument  is "mkdir <dirname>"*/
-    fs::create_dir(name);
-}
-fn do_rmdir(name: &str){
-    fs::remove_dir(name).unwrap_or_else( |_|{
-        println!("rmdir: cant delete {}{}",name,"No such file or directory");
-    });
-}
-fn do_touch(filename: &str){
-    fs::File::create(filename);
-}
-fn do_rm(filename: &str){
-    fs::remove_file(filename).unwrap_or_else(|_|{
-        println!("rm: cant delete {}{}",filename,"No such file or directory");
-    });
-}
-fn do_echo(input: &str){
-    if input.starts_with("$"){
-        let path =  input.replace("$","");
-        let out_dir = env::var(path).unwrap();
-        print!("{}",out_dir);        
-    }else {
-        print!("{}",input);        
-    }
-}
+mod functions;
+mod test_parse;
+
 named!(echo_command_parser<&str,&str>,
     ws!(tag_s!("echo "))
 );
@@ -80,66 +31,66 @@ named!(cat_command_parser<&str,&str>,
 named!(rm_command_parser<&str,&str>,
     ws!(tag_s!("rm "))
 );
-fn  excute_command(input: &str){
-  if input.starts_with("ls"){
-        get_ls();
-    }else if input.starts_with("pwd"){
-        get_pwd();
-    }else if input.starts_with("mkdir"){
-        match mkdir_command_parser(input){
-            IResult::Done(operand, _) => do_mkdir(operand),
-            IResult::Error(error) => println!("Error: {:?}", error),
-            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
-        }
-    }else if input.starts_with("rmdir"){
-        match rmdir_command_parser(input){
-            IResult::Done(operand, _) => do_rmdir(operand),
-            IResult::Error(error) => println!("Error: {:?}", error),
-            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
-        }
-    }else if input.starts_with("echo") {
-        match echo_command_parser(input) {
-            IResult::Done(operand, _) => do_echo(operand),
-            IResult::Error(_) => println!(""),
-            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
-        }
-    }else if input.starts_with("curl"){
-        match curl_command_parser(input) {
-            IResult::Done(url, _) => curl_get_request(url),
-            IResult::Error(_) => println!("curl: usage: curl [URL]"),
-            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
-        }
-    }else if input.starts_with("rm"){
-        match rm_command_parser(input){
-            IResult::Done(filename, _) => do_rm(filename),
-            IResult::Error(_) => println!("there isnt operand"),
-            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
-        }
-    }else if input.starts_with("touch"){
-        match touch_command_parser(input){
-            IResult::Done(filename, _) => do_touch(filename),
-            IResult::Error(_) => println!("there isnt operand"),
-            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
-        }
-    }else if input.starts_with("cat"){
-        match cat_command_parser(input){
-            IResult::Done(operand, _) => do_cat(operand),
-            IResult::Error(error) => println!("Error: {:?}", error),
-            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
-        }
-    }
-    else {
-        println!("This command is unimplemented now. Sry :o");
+
+fn handle_echo_command(input:&str){
+    match echo_command_parser(input) {
+        IResult::Done(operand, _) => functions::do_echo(operand),
+        IResult::Error(_) => println!(""),
+        IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
     }
 }
-#[test]
-fn test_parse_greeting(){
-    excute_command("echo Hello");
-    excute_command("mkdir a");
-    excute_command("ls");
-    excute_command("Hello");
+fn handle_curl_command(input:&str){
+
 }
-fn string_to_static_str(s: String) -> &'static str { // String convert to static str
+fn handle_touch_command(input:&str){
+
+}
+fn handle_rm_command(input:&str){
+
+}
+fn handle_mkdir_command(input:&str){
+    match mkdir_command_parser(input){
+        IResult::Done(operand, _) => functions::do_mkdir(operand),
+        IResult::Error(error) => println!("Error: {:?}", error),
+        IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
+    }
+}
+fn handle_rmdir_command(input:&str){
+    match rmdir_command_parser(input){
+            IResult::Done(operand, _) => functions::do_rmdir(operand),
+            IResult::Error(error) => println!("Error: {:?}", error),
+            IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
+    }
+}
+fn handle_cat_command(input:&str){
+
+}
+
+fn tokenize_command(input: &str){
+    let separators = vec![' ', '\n', '\t', '\r'];
+    let mut tokenizer = token::Tokenizer::new(input.as_bytes(), separators);
+
+    loop {
+        let token = match tokenizer.next().unwrap() {
+            Some(token) => token,
+            None => { break; }
+        };
+        match token {
+            "echo"   => handle_echo_command(input),
+            "curl"   => handle_curl_command(input),
+            "touch"  => handle_touch_command(input),
+            "rm"     => handle_rm_command(input),
+            "mkdir"  => handle_mkdir_command(input),
+            "rmdir"  => handle_rmdir_command(input),
+            "cat"    => handle_cat_command(input),
+            "ls"     => functions::get_ls(),
+            "pwd"    => functions::get_pwd(),
+            _=> println!("")
+        }
+    }
+}
+
+fn string_to_static_str(s: String) -> &'static str { //  ReadLine's String convert to static str
     unsafe {
         let ret = mem::transmute(&s as &str);
         mem::forget(s);
@@ -151,9 +102,21 @@ fn init_dummy_shell(){
         let mut standard_input = String::new();
         io::stdin().read_line(&mut standard_input).expect("Failed to read line");
         let input_to_excuter = string_to_static_str(standard_input);
-        excute_command(input_to_excuter);
+   //     excute_command(input_to_excuter);
     }
 }
 fn main() {
-    init_dummy_shell();
+    test_parse::call();
+}
+
+#[test]
+fn test_tokenize_command(){
+    tokenize_command("echo Hoge");
+    tokenize_command("Hi! Im 17 guy. I like to play internet game")
+}
+#[test]
+fn call_module_function(){
+    functions::do_echo("Function called!");
+    functions::curl_get_request("http://example.com");    
+    functions::get_ls();    
 }
--- src/test_parse.rs
+++ src/test_parse.rs
@@ -1,27 +1,20 @@
 #[macro_use]
+#[allow(unused_must_use)]
+#[warn(unused_must_use)]
 extern crate nom;
-
 use nom::{IResult, space, alpha};
 
-
-fn test4(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    tag!(input, "Hello,") => print(), 
-}
+named!(command_parser<&str,&str>,
+    ws!(tag_s!("Hello, "))
+);
 fn print(){
     println!("aa");
 }
 
-fn main() {
-    let sample = "Hello, World!";
-
-    let tes: &[u8] = &[0; 4];
-
-    match test4(sample.as_bytes()) {
-        IResult::Done(i,_) => {
-            println!(
-                "{}",
-                String::from_utf8(i.to_vec()).unwrap())
-        }
-        _ => println!("Other!\n"),
-    };
+pub fn call() {
+    match command_parser("Hello,"){
+        IResult::Done(operand, _) => print(),
+        IResult::Error(error) => println!("Error: {:?}", error),
+        IResult::Incomplete(needed) => println!("Incomplete: {:?}", needed)
+    }
 }
